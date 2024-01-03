e26d5e25ceac3a2ed31f681ecc05f4a4e36eb58f
Durganshu Mishra <60805696+Durganshu@users.noreply.github.com>
2024-01-03 04:12:00 +0100

    Update search-minmax.cpp

--- search-minmax.cpp
+++ search-minmax.cpp
@@ -1,9 +1,11 @@
 /**
- * Very simple example strategy:
- * Search all possible positions reachable via one move,
- * and return the move leading to best position
+ * 
+ * Implementation of sequential MiniMax strategy 
+ * Authors:
+ * - Aditya Phopale
+ * - Durganshu Mishra
+ * - Gaurav Gokhale
  *
- * (c) 2006, Josef Weidendorfer
  */
 
 #include "search.h"
@@ -12,34 +14,15 @@
 #include <iostream>
 #include <algorithm>
 
-/**
- * To create your own search strategy:
- * - copy this file into another one,
- * - change the class name one the name given in constructor,
- * - adjust clone() to return an instance of your class
- * - adjust last line of this file to create a global instance
- *   of your class
- * - adjust the Makefile to include your class in SEARCH_OBJS
- * - implement searchBestMove()
- *
- * Advises for implementation of searchBestMove():
- * - call foundBestMove() when finding a best move since search start
- * - call finishedNode() when finishing evaluation of a tree node
- * - Use _maxDepth for strength level (maximal level searched in tree)
- */
 class MinMaxStrategy : public SearchStrategy
 {
 public:
-    // Defines the name of the strategy
     MinMaxStrategy() : SearchStrategy("MinMax") {}
 
-    // Factory method: just return a new instance of this class
     SearchStrategy *clone() { return new MinMaxStrategy(); }
 
 private:
-    /**
-     * Implementation of the strategy.
-     */
+
     void searchBestMove();
 
     int MinMax(int currentdepth);
